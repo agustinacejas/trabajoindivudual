00e0ac361a4771274dfd0549745a071ac5168965
agustinacejas <aguceza70@gmail.com>
2022-08-12 16:10:47 -0300

    Entrega 1

---
+++ js/login.js
@@ -0,0 +1,18 @@
+function login() {
+    let usuario = document.getElementById("nombre").value;
+    let clave = document.getElementById("clave").value;
+
+    if (usuario != "" && clave != "") {
+        localStorage.setItem('user',usuario);
+        location.href = "index.html";
+    } else {
+        alert("Usuario y clave son obligatorios");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("inicio").addEventListener("click", () =>{
+        login();
+    });
+});
---
+++ js/products.js
@@ -0,0 +1,41 @@
+let categoriaArray = [];
+
+
+function mostrarAutos (array){
+    let auto = "";
+    
+    for(let i = 0; i < array.products.length; i++) {
+        let products = array.products[i];
+        auto += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ products.name +`</h4> 
+                        <p> `+ products.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + products.cost + products.currency + `</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("aca").innerHTML = auto; 
+    }  
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(LIST_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            console.log(resultObj.data)
+            categoriaArray = resultObj.data;
+            mostrarAutos(categoriaArray);
+        }
+    });
+});
